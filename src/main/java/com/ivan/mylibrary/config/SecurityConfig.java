b3611b1db508da44499ec5d2fa7fe7e3e5cca192
Ivan Ruzhalovich <ruzhalovich@bk.ru>
2025-02-18 11:08:07 +0300

    Add security

---
+++ src/main/java/com/ivan/mylibrary/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ivan.mylibrary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> request.requestMatchers("/books/**")
+                        .permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager
+                .createUser(User.withUsername("Ivan")
+                .password("password")
+                .authorities("USER_ROLE").build());
+        return userDetailsManager;
+    }
+}
